513b25c7e7072f788ac8b8cadec632ac56a0bdc1
foxontheerun <bulatovaalsu22@gmail.com>
2022-04-18 21:22:09 +0300

    color 2

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Titles = () => (
 	<div>
-		<h1 className="title-container__title">Узнай температуру в любом городе мира!</h1>
+		<h1 className="title-container__title">Узнай температуру воздуха в любом городе мира!</h1>
 		
 	</div>
 );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,24 +2,11 @@ import React from "react";
 
 const Weather = props => (
 	<div className="weather__info">
-	 {/* {	
-	 	props.city && props.country && <p className="weather__key"> Место: 
-	 		<span className="weather__value"> { props.city }, { props.country }</span>
-	 	</p> 
-	 } */}
 	 { 	
 	 	props.temperature && <p className="weather__key"> Температура: 
 	 		<span className="weather__value"> { props.temperature }	C</span>
 	 	</p> 
 	 }
-	 {/* { 	
-	 	props.humidity && <p className="weather__key"> Влажность: 
-	 		<span className="weather__value"> { props.humidity } %</span>
-	 	</p> 
-	 }
-	 { 
-	 	props.error && <p className="weather__error">{ props.error }</p>  
-	 } */}
 	</div>
 );
 
